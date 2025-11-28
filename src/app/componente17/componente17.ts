bef59e12707b1d93b66c5e23803c4db4c49c1e5a
Eduardo <rreduardo2006@gmail.com>
2025-11-28 09:49:57 +0100

    Primer cambio en feature

---
+++ src/app/componente17/componente17.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente17',
+  imports: [],
+  templateUrl: './componente17.html',
+  styleUrl: './componente17.css'
+})
+export class Componente17 {
+
+}
