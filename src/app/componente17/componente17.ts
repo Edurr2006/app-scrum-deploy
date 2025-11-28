33b7d79a8a2e7d0ef70368adaec38c0a5cb38c87
Eduardo <rreduardo2006@gmail.com>
2025-11-28 10:02:13 +0100

    errores feature fixed

--- src/app/componente17/componente17.ts
+++ src/app/componente17/componente17.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-componente17',
+  standalone: true,
   imports: [],
   templateUrl: './componente17.html',
-  styleUrl: './componente17.css'
+  styleUrls: ['./componente17.css']
 })
 export class Componente17 {
 
