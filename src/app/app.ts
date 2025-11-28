9a04f9b1dd3710ff2dd122235fe1940b62a275ee
Eduardo <rreduardo2006@gmail.com>
2025-11-28 10:07:03 +0100

    fix feature definitivo

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Componente17 } from './componente17/componente17.js';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Componente17],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
